563735cee27ffbe24108ba3f237a5d1d2cf56cba
enestanrikuluu <enestanrikulu2002@gmail.com>
2024-02-15 01:23:37 +0300

    nodemon

--- day4747/app.js
+++ day4747/app.js
@@ -1,13 +1,43 @@
+const fs = require('fs');
+
+const path = require('path');
 const express = require('express');
 
 const app = express();
+
+app.use(express.urlencoded({extended: false}));
 
 app.get('/currenttime', function(req, res) {
     res.send('<h1>'+ new Date().toISOString() +'</h1>');
 });
 
 app.get('/', function(req, res) {
-    res.send('<form action ="/store-user" method="POST"><label>Your Name</label><input type="text"><button></button></form>');
+    res.send('<form action ="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button></button></form>');
+});
+
+
+app.post('/store-user', function(req, res) {
+    console.log(req.body.username);
+    const filePath = path.join(__dirname, 'data', 'users.json');
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+    existingUsers.push(req.body.username);
+    fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+    res.send('<h1>Form Submitted</h1>');
+});
+
+
+app.get('/users', function(req, res) {
+    const filePath = path.join(__dirname, 'data', 'users.json');
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+    let response = '<ul>';
+    for (const user of existingUsers) {
+        response += '<li>'+user+'</li>';
+    }
+    response += '</ul>';
+    res.send(response);
+
 });
 
 
