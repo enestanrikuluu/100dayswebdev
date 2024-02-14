5180bd9ddb5741e7550d9201939c149fa59b822e
enestanrikuluu <enestanrikulu2002@gmail.com>
2024-02-14 20:13:25 +0300

    express.js

--- day4747/app.js
+++ day4747/app.js
@@ -1,1 +1,48 @@
-console.log("hello")
+const express = require('express');
+
+const app = express();
+
+app.get('/currenttime', function(req, res) {
+    res.send('<h1>'+ new Date().toISOString() +'</h1>');
+});
+
+app.get('/', function(req, res) {
+    res.send('<form action ="/store-user" method="POST"><label>Your Name</label><input type="text"><button></button></form>');
+});
+
+
+ // localhost:3000/currenttime
+app.listen(3000);
+
+// function requestHandler(req, res) {
+
+//     if (req.url === '/currenttime') {
+//         res.statusCode = 200;
+//         res.end('<h1>'+ new Date().toISOString() +'</h1>');
+//     }
+
+//     if (req.url === '/about') {
+//         res.statusCode = 200;
+//         res.end('<h1>About Us</h1>');
+//         return;
+//     }
+//     if (req.url === '/contact') {
+//         res.statusCode = 200;
+//         res.end('<h1>Contact Us</h1>');
+//         return;
+//     }
+
+//     if (req.url === '/date') {
+//         res.statusCode = 200;
+//         res.end('<h1>'+ new Date().toISOString() +'</h1>');
+//         return;
+//     }
+
+//     res.statusCode = 200;
+//     res.end('<h1>Hello World</h1>');
+// }
+
+
+// const server = http.createServer(requestHandler);
+// server.listen(3000);
+
